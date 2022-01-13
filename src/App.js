2469510fbfa1c0c1b21472b0c22de85795ab61b6
Woodstock <victorhe97@gmail.com>
2022-01-12 22:26:50 -0500

    Added doge logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import logo from './logo.svg';
+import doge from './assets/header-doge.jpg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import AllDogBreeds from './components/AllDogBreeds';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <nav className='App-nav'>
-          <div>ALL DOG BREEDS!</div>
-          <div>And maybe some more stuff</div>
-        </nav>
-      </header>
-      <main>
-        <AllDogBreeds />
-      </main>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={doge} alt='doge-logo' />
+          <nav className='App-nav'>
+            <div>ALL DOG BREEDS!</div>
+            <div>And maybe some more stuff</div>
+          </nav>
+        </header>
+        <main>
+          <AllDogBreeds />
+        </main>
+      </div>
+    </Router>
   );
 }
 
