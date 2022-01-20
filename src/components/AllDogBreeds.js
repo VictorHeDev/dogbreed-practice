7d83c0c9101aab56f21fc822b78ea350637fe8b8
Woodstock <victorhe97@gmail.com>
2022-01-20 16:40:23 -0500

    Clean up useEffect hook using async await syntax

--- src/components/AllDogBreeds.js
+++ src/components/AllDogBreeds.js
@@ -6,23 +6,35 @@ const AllDogBreeds = () => {
   const BASE_URL = `https://dog.ceo/api/breeds/list/all`;
   const [allDogsData, setAllDogsData] = useState(null);
 
+  // useEffect(() => {
+  //   const getDogBreeds = async () => {
+  //     const response = await axios
+  //       .get(BASE_URL)
+  //       .then((promise) => {
+  //         return promise.data;
+  //       })
+  //       .catch((e) => {
+  //         console.log('The error is: ', e);
+  //       });
+  //     // call get dog breeds from useEffect
+  //     // call set dog breeds within the async function
+  //     setAllDogsData(response.message);
+  //   };
+  //   getDogBreeds();
+  //   // setDogBreeds(getDogBreeds());
+  //   // const allDogs = Object.values(dogBreeds);
+  // }, [BASE_URL]);
+
   useEffect(() => {
     const getDogBreeds = async () => {
-      const response = await axios
-        .get(BASE_URL)
-        .then((promise) => {
-          return promise.data;
-        })
-        .catch((e) => {
-          console.log('The error is: ', e);
-        });
-      // call get dog breeds from useEffect
-      // call set dog breeds within the async function
-      setAllDogsData(response.message);
+      try {
+        const response = await axios.get(BASE_URL);
+        setAllDogsData(response.data.message);
+      } catch (e) {
+        console.log('The error is: ', e);
+      }
     };
     getDogBreeds();
-    // setDogBreeds(getDogBreeds());
-    // const allDogs = Object.values(dogBreeds);
   }, [BASE_URL]);
 
   const renderAllDogs = () => {
@@ -46,9 +58,6 @@ const AllDogBreeds = () => {
       })
     );
   };
-
-  // if (!allDogsData) return null;
-  // console.log(allDogsData);
 
   return (
     <>
